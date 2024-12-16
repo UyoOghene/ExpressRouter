ff327c5a80167457bf6804de04fcfeed1b755942
Uyooghene <uyooghene01@gmail.com>
2024-12-16 12:16:44 +0100

    m

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const app = express();
 const shelterRoutes = require('./Routes/shelters');
 const dogRoutes = require('./Routes/dogs');
 const adminRoutes = require('./Routes/admin');
+const cookieParser = require('cookie-parser');
 
 
+app.use(cookieParser());
 app.use('/shelters', shelterRoutes);
 app.use('/dogs', dogRoutes);
 app.use('/admin', adminRoutes);
+
+
+app.get('/greet',(req,res)=> {
+    console.log(req.cookies)
+    res.send('greet')
+})
+
+app.get('/setnamecookies',(req,res)=>{
+    res.cookie('name','hetta')
+    res.cookie('book', 'harry')
+    res.send('hi')
+})
 
 app.listen(3000, ()=>{
     console.log('lstening on 3000')
