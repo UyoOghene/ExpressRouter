d6157b76e0e1b75afb163eaab7ebf1ff9c9f907b
Uyooghene <uyooghene01@gmail.com>
2024-12-18 12:05:28 +0100

    cookiesigning

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ const adminRoutes = require('./Routes/admin');
 const cookieParser = require('cookie-parser');
 
 
-app.use(cookieParser());
+app.use(cookieParser('secret'));
 app.use('/shelters', shelterRoutes);
 app.use('/dogs', dogRoutes);
 app.use('/admin', adminRoutes);
 
 
 app.get('/greet',(req,res)=> {
-    console.log(req.cookies)
-    res.send('greet')
+    // const {name,book} = req.cookies;
+
+    // console.log(req.cookies)
+    res.send(req.cookies)
 })
 
 app.get('/setnamecookies',(req,res)=>{
@@ -22,6 +24,19 @@ app.get('/setnamecookies',(req,res)=>{
     res.cookie('book', 'harry')
     res.send('hi')
 })
+
+app.get('/signedcookie', (req,res)=>{
+    res.cookie('fruit','grape', {signed: true});
+    res.cookie('movie','twilight', {signed: true});
+    res.send('sent fruit')
+})
+app.get('/verifysignedcookie', (req,res)=>{
+    // console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
+})
+
+
 
 app.listen(3000, ()=>{
     console.log('lstening on 3000')
